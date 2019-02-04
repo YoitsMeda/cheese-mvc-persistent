1a867a310b537a1e89648963e43af3cdb9df3b66
Yemeda Grayson <42628679+YoitsMeda@users.noreply.github.com>
2019-02-04 16:55:04 -0600

    Add files via upload

---
+++ cheese-mvc-persistent-master/src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,28 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Category 
+{
+@Id
+@GeneratedValue
+private int id;
+
+@NotNull
+@Size(min=3, max=15)
+private String name;
+
+public Category() { } 
+
+	
+
+	
+	
+
+}//END CLASS
+
